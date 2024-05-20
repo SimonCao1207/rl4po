bc5c2ff92ee1508f01972f395a60445e9538a846
vzsky <my999n@gmail.com>
2024-05-20 16:25:11 +0900

    refine mvo

--- mvo.py
+++ mvo.py
@@ -4,16 +4,32 @@ from pypfopt import risk_models
 from pypfopt import EfficientFrontier
 from pypfopt import expected_returns
 
-prices = pd.read_csv("data/return_df.csv", index_col="Date")
+RISKFREE_RATE = 0.03
+
+prices = pd.read_csv("data/snp500_index.csv", index_col="Date")
+prices = prices[['Open']]
 
 # Calculate expected returns and sample covariance
 mu = expected_returns.mean_historical_return(prices)
 S = risk_models.CovarianceShrinkage(prices).ledoit_wolf()
 
-# Optimize for maximal Sharpe ratio
+# Optimize for maximal Sharpe ratio, which is the tangential portfolio
 ef = EfficientFrontier(mu, S)
-raw_weights = ef.max_sharpe()
+raw_weights = ef.max_sharpe(RISKFREE_RATE)
 cleaned_weights = ef.clean_weights()
 ef.save_weights_to_file("data/weights.csv")  # saves to file
 print(cleaned_weights)
-ef.portfolio_performance(verbose=True)
+
+ef.portfolio_performance(verbose=True, risk_free_rate=RISKFREE_RATE)
+
+mu = expected_returns.mean_historical_return(prices)
+S = risk_models.CovarianceShrinkage(prices).ledoit_wolf()
+
+# Optimize for maximal Sharpe ratio, which is the tangential portfolio
+ef = EfficientFrontier(mu, S)
+raw_weights = ef.max_sharpe(RISKFREE_RATE)
+cleaned_weights = ef.clean_weights()
+ef.save_weights_to_file("data/weights.csv")  # saves to file
+print(cleaned_weights)
+
+ef.portfolio_performance(verbose=True, risk_free_rate=RISKFREE_RATE)
