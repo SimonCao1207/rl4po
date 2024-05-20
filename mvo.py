f50f57e3647883d56f7ac3ae36efc958dafac7de
vzsky <my999n@gmail.com>
2024-05-20 16:55:42 +0900

    misunderstanding: rollback

--- mvo.py
+++ mvo.py
@@ -6,8 +6,7 @@ from pypfopt import expected_returns
 
 RISKFREE_RATE = 0.03
 
-prices = pd.read_csv("data/snp500_index.csv", index_col="Date")
-prices = prices[['Open']]
+prices = pd.read_csv("data/return_df.csv", index_col="Date")
 
 # Calculate expected returns and sample covariance
 mu = expected_returns.mean_historical_return(prices)
@@ -22,14 +21,3 @@ print(cleaned_weights)
 
 ef.portfolio_performance(verbose=True, risk_free_rate=RISKFREE_RATE)
 
-mu = expected_returns.mean_historical_return(prices)
-S = risk_models.CovarianceShrinkage(prices).ledoit_wolf()
-
-# Optimize for maximal Sharpe ratio, which is the tangential portfolio
-ef = EfficientFrontier(mu, S)
-raw_weights = ef.max_sharpe(RISKFREE_RATE)
-cleaned_weights = ef.clean_weights()
-ef.save_weights_to_file("data/weights.csv")  # saves to file
-print(cleaned_weights)
-
-ef.portfolio_performance(verbose=True, risk_free_rate=RISKFREE_RATE)
